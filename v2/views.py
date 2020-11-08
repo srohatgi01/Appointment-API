e6dd740aea4c6233bc526bddd2c2d0b86bc5e9c8
Sarthak Rohatgi <infinitybusiness726@gmail.com>
2020-11-08 08:11:26 -0800

    removed filters and pagination file

--- v2/views.py
+++ v2/views.py
@@ -4,7 +4,6 @@ from rest_framework.parsers import JSONParser
 from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
 from .models import *
 from .serializers import *
-from .filters import *
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, viewsets, generics
